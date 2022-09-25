381000b021d49505a70c2df17d7ce35a71fe34e9
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-09-25 22:06:46 +0900

    Update 03_order_by_220925.sql

--- 01_데이터_분석을_위한_기초_SQL/03_order_by_220925.sql
+++ 01_데이터_분석을_위한_기초_SQL/03_order_by_220925.sql
@@ -34,15 +34,17 @@ FROM Employee
 WHERE salary > 2000 AND months < 10
 ORDER BY employee_id;
 
-/* Q3. Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+/* Q3. Higher Than 75 Marks)
+Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
 If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
 SELECT Name
 FROM STUDENTS
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID;
 
-/* Q4. Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than. 
- Round your answer to  decimal places. */
+/* Q4. Weather Observation Station 15)
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than. 
+Round your answer to  decimal places. */
 SELECT ROUND(LONG_W, 4)
 FROM STATION
 WHERE LAT_N < 137.2345 
