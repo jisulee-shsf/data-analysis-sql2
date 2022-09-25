4a602a61cb560c7602b1784d59c292247ec43b72
Jisu Lee <jisulee.shsf@gmail.com>
2022-09-25 13:21:20 +0900

    Upload file

---
+++ 01_데이터_분석을_위한_기초_SQL/01_select_from_limit_220925.sql
@@ -0,0 +1,17 @@
+-- [Section 1] SELECT / FROM / LIMIT
+
+-- Q. 'LIMIT'를 사용해 전체 데이터의 상위 10개 값 추출하기
+-- *: 아스타(Asterisk)
+SELECT *
+FROM Customers
+LIMIT 10;
+
+-- [Section 2] HackerRank
+
+-- Q1. Select All) Query all columns (attributes) for every row in the CITY table.
+SELECT * 
+FROM CITY;
+
+-- Q2. Weather Observation Station 1) Query a list of CITY and STATE from the STATION table.
+SELECT CITY, STATE 
+FROM STATION;
---
+++ 01_데이터_분석을_위한_기초_SQL/README.md
@@ -0,0 +1,7 @@
+####  
+## 01. 데이터 분석을 위한 기초 SQL
+#### ► [01_select_from_limit_220925]  
+- SELECT / FROM / LIMIT 활용 실습 및 HackerRank 문제 풀이
+- Select All) https://www.hackerrank.com/challenges/select-all-sql/problem
+- Weather Observation Station 1) https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+####  
