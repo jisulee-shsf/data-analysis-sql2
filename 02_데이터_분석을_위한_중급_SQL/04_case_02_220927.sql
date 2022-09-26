d63eb83d75c7af6bc25a932ef97a176a21a22f5f
Jisu Lee <jisulee.shsf@gmail.com>
2022-09-27 07:02:11 +0900

    Upload file

---
+++ 02_데이터_분석을_위한_중급_SQL/04_case_02_220927.sql
@@ -0,0 +1,22 @@
+-- Q1. Pivot Table) CASE WHEN을 사용해 SupplierID별 Price 총합 출력하기
+SELECT SUM(CASE WHEN SupplierID = 1 THEN Price ELSE NULL END) AS 'SupplierID1_avg'
+	 , SUM(CASE WHEN SupplierID = 2 THEN Price ELSE NULL END) AS 'SupplierID2_avg'
+     , SUM(CASE WHEN SupplierID = 3 THEN Price ELSE NULL END) AS 'SupplierID3_avg'
+FROM Products;
+
+-- Q2. Pivot Table) CASE WHEN을 사용해 month별 revenue 총합을 id에 따라 출력하기
+SELECT id
+     , SUM(CASE WHEN month = 'Jan' THEN revenue ELSE NULL END) AS Jan_Revenue
+     , SUM(CASE WHEN month = 'Feb' THEN revenue ELSE NULL END) AS Feb_Revenue
+     , SUM(CASE WHEN month = 'Mar' THEN revenue ELSE NULL END) AS Mar_Revenue
+     , SUM(CASE WHEN month = 'Apr' THEN revenue ELSE NULL END) AS Apr_Revenue
+     , SUM(CASE WHEN month = 'May' THEN revenue ELSE NULL END) AS May_Revenue
+     , SUM(CASE WHEN month = 'Jun' THEN revenue ELSE NULL END) AS Jun_Revenue
+     , SUM(CASE WHEN month = 'Jul' THEN revenue ELSE NULL END) AS Jul_Revenue
+     , SUM(CASE WHEN month = 'Aug' THEN revenue ELSE NULL END) AS Aug_Revenue
+     , SUM(CASE WHEN month = 'Sep' THEN revenue ELSE NULL END) AS Sep_Revenue
+     , SUM(CASE WHEN month = 'Oct' THEN revenue ELSE NULL END) AS Oct_Revenue
+     , SUM(CASE WHEN month = 'Nov' THEN revenue ELSE NULL END) AS Nov_Revenue
+     , SUM(CASE WHEN month = 'Dec' THEN revenue ELSE NULL END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
