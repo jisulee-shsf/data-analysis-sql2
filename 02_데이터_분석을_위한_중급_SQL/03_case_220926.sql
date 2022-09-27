d29faa03b30ea997b94478a67a7789fa81185ac0
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-09-27 14:11:40 +0900

    Update and rename 03_case_01_220926.sql to 03_case_220926.sql

--- 02_데이터_분석을_위한_중급_SQL/03_case_220926.sql
+++ 02_데이터_분석을_위한_중급_SQL/03_case_220926.sql
@@ -26,7 +26,13 @@ SELECT CASE
 FROM Products
 GROUP BY Case_test;
 
--- [Section 2] HakerRank
+/* Q4. Pivot Table) CASE WHEN을 사용해 SupplierID별 Price 총합 출력하기 */
+SELECT SUM(CASE WHEN SupplierID = 1 THEN Price ELSE NULL END) AS 'SupplierID1_avg'
+     , SUM(CASE WHEN SupplierID = 2 THEN Price ELSE NULL END) AS 'SupplierID2_avg'
+     , SUM(CASE WHEN SupplierID = 3 THEN Price ELSE NULL END) AS 'SupplierID3_avg'
+FROM Products;
+
+-- [Section 2] HakerRank / LeetCode
 
 /* Q1. Type of Triangle)
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
@@ -42,3 +48,22 @@ SELECT CASE
             ELSE 'Scalene'
        END
 FROM TRIANGLES;
+
+/* Q2. 1179. Reformat Department Table)
+Write an SQL query to reformat the table such that there is a department id column and a revenue column for each month.
+Return the result table in any order. */
+SELECT id
+     , SUM(CASE WHEN month = 'Jan' THEN revenue ELSE NULL END) AS Jan_Revenue
+     , SUM(CASE WHEN month = 'Feb' THEN revenue ELSE NULL END) AS Feb_Revenue
+     , SUM(CASE WHEN month = 'Mar' THEN revenue ELSE NULL END) AS Mar_Revenue
+     , SUM(CASE WHEN month = 'Apr' THEN revenue ELSE NULL END) AS Apr_Revenue
+     , SUM(CASE WHEN month = 'May' THEN revenue ELSE NULL END) AS May_Revenue
+     , SUM(CASE WHEN month = 'Jun' THEN revenue ELSE NULL END) AS Jun_Revenue
+     , SUM(CASE WHEN month = 'Jul' THEN revenue ELSE NULL END) AS Jul_Revenue
+     , SUM(CASE WHEN month = 'Aug' THEN revenue ELSE NULL END) AS Aug_Revenue
+     , SUM(CASE WHEN month = 'Sep' THEN revenue ELSE NULL END) AS Sep_Revenue
+     , SUM(CASE WHEN month = 'Oct' THEN revenue ELSE NULL END) AS Oct_Revenue
+     , SUM(CASE WHEN month = 'Nov' THEN revenue ELSE NULL END) AS Nov_Revenue
+     , SUM(CASE WHEN month = 'Dec' THEN revenue ELSE NULL END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
