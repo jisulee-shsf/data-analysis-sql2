ac60607e69786eabde8126fec1df59c5246834ba
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-09-27 14:07:22 +0900

    Update 06_left_join_220927.sql

--- 02_데이터_분석을_위한_중급_SQL/06_left_join_220927.sql
+++ 02_데이터_분석을_위한_중급_SQL/06_left_join_220927.sql
@@ -1,19 +1,19 @@
--- [Section 1] OUTER JOIN 
+-- [Section 1] LEFT JOIN 
 
 -- Q1-1. Customers & Orders table을 key 컬럼 기준으로 LEFT OUTER JOIN 실행하기
 SELECT *
 FROM Customers a 
-		LEFT OUTER JOIN Orders b ON a.CustomerID = b.CustomerID;
+     LEFT OUTER JOIN Orders b ON a.CustomerID = b.CustomerID;
 		
 -- Q1-2. Customers & Orders table을 key 컬럼 기준으로 LEFT JOIN 실행하기
 SELECT *
 FROM Customers a 
-		LEFT JOIN Orders b ON a.CustomerID = b.CustomerID;
+     LEFT JOIN Orders b ON a.CustomerID = b.CustomerID;
 
 -- Q2. LEFT JOIN을 사용해 구매 내역이 없는 인원 수 출력하기	
 SELECT COUNT(*)
 FROM Customers a 
-		LEFT JOIN Orders b ON a.CustomerID = b.CustomerID
+     LEFT JOIN Orders b ON a.CustomerID = b.CustomerID
 WHERE OrderID IS NULL;
 
 -- [Section 2] LeetCode
@@ -23,5 +23,5 @@ Write an SQL query to report all customers who never order anything.
 Return the result table in any order. */
 SELECT Customers.NAME AS Customers
 FROM Customers
-        LEFT JOIN Orders ON Customers.id = Orders.customerId
+     LEFT JOIN Orders ON Customers.id = Orders.customerId
 WHERE customerId IS NULL;
