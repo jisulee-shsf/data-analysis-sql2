5807f3874c19285835a5580e691d1c7f70659f35
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-09-29 16:30:45 +0900

    Update 03_test_220929.sql

--- 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
+++ 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
@@ -18,7 +18,7 @@ OR CITY NOT LIKE '%a'
 OR CITY NOT LIKE '%e' 
 OR CITY NOT LIKE '%i' 
 OR CITY NOT LIKE '%o' 
-OR CITY NOT LIKE '%u'
+OR CITY NOT LIKE '%u';
 
 -- A2-1. 수정 코드
 SELECT DISTINCT CITY
@@ -32,24 +32,26 @@ OR CITY NOT LIKE '%a'
 AND CITY NOT LIKE '%e' 
 AND CITY NOT LIKE '%i' 
 AND CITY NOT LIKE '%o' 
-AND CITY NOT LIKE '%u'
+AND CITY NOT LIKE '%u';
 
 -- A2-2. 수정 코드
 SELECT DISTINCT CITY
 FROM STATION
 WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u') 
-OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u') 
+OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
 
 -- Q3. Weather Observation Station 13
 /* Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
- * Truncate your answer to  decimal places. */
+Truncate your answer to  decimal places. */
 SELECT TRUNCATE(SUM(LAT_N), 4) 
 FROM STATION
-WHERE LAT_N > 38.7880 AND LAT_N < 137.2345
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
 
--- Q4. Weather Observation Station 13
-/* Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
- * Truncate your answer to  decimal places. */
+-- Q4. Top Competitors)
+/* Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
+Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
+If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id. */
 -- A. 수정 코드
 SELECT S.hacker_id
      , H.name
@@ -60,4 +62,4 @@ FROM Submissions S
 WHERE S.score = D.score
 GROUP BY S.hacker_id, H.name
 HAVING COUNT(DISTINCT S.submission_id) >= 2
-ORDER BY COUNT(DISTINCT S.submission_id) DESC, S.hacker_id
+ORDER BY COUNT(DISTINCT S.submission_id) DESC, S.hacker_id;
