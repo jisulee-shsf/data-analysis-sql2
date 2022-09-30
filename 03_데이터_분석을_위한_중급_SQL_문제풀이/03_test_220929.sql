78cb450888ab87943ed5892e3f1b2453fa17740c
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-01 02:57:04 +0900

    Update 03_test_220929.sql

--- 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
+++ 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
@@ -37,12 +37,12 @@ AND CITY NOT LIKE '%u';
 -- A2-2. 수정 코드
 SELECT DISTINCT CITY
 FROM STATION
-WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u') 
+WHERE LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') 
 OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
 
--- Q3. Weather Observation Station 13
+-- Q3. Weather Observation Station 13)
 /* Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
-Truncate your answer to  decimal places. */
+Truncate your answer to 4 decimal places. */
 SELECT TRUNCATE(SUM(LAT_N), 4) 
 FROM STATION
 WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
