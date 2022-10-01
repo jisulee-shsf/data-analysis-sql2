0ef9a2d060ae7abe5755c1da6e5d5305ca9c2886
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-01 19:55:26 +0900

    Update 01_test_220928.sql

--- 03_데이터_분석을_위한_중급_SQL_문제풀이/01_test_220928.sql
+++ 03_데이터_분석을_위한_중급_SQL_문제풀이/01_test_220928.sql
@@ -1,7 +1,7 @@
 -- Q1. 595. Big Countries)
 /* A country is big if:
-it has an area of at least three million (i.e., 3000000 km2), or
-it has a population of at least twenty-five million (i.e., 25000000).
+- it has an area of at least three million (i.e., 3000000 km2), or
+- it has a population of at least twenty-five million (i.e., 25000000).
 Write an SQL query to report the name, population, and area of the big countries.
 Return the result table in any order. */
 SELECT name, population, area
