c84e1cb4cebf2709d99bdbe628680ed833c382a2
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-21 05:07:31 +0900

    Update 03_order_by_220925.sql

--- 01_데이터_분석을_위한_기초_SQL/03_order_by_220925.sql
+++ 01_데이터_분석을_위한_기초_SQL/03_order_by_220925.sql
@@ -1,18 +1,18 @@
 -- [Section 1] ORDER BY
 
--- Q1. PRICE가 50보다 큰 물품을 내림차순으로 정렬하기
+# PRICE가 50보다 큰 물품을 내림차순으로 정렬하기
 SELECT * 
 FROM Products
 WHERE PRICE > 50
 ORDER BY PRICE DESC;
 
--- Q2. PRICE가 가장 낮은 물품 출력하기
+# PRICE가 가장 낮은 물품 출력하기
 SELECT * 
 FROM Products
 ORDER BY PRICE
 LIMIT 1;
 
--- Q3. PRICE가 가장 높은 물품 출력하기
+# PRICE가 가장 높은 물품 출력하기
 SELECT * 
 FROM Products
 ORDER BY PRICE DESC
@@ -20,13 +20,13 @@ LIMIT 1;
 
 -- [Section 2] HackerRank
 
-/* Q1. Employee Names)
+/* Employee Names -
 Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order. */
 SELECT name
 FROM Employee
 ORDER BY;
 
-/* Q2. Employee Salaries)
+/* Employee Salaries -
 Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. 
 Sort your result by ascending employee_id. */
 SELECT name
@@ -34,7 +34,7 @@ FROM Employee
 WHERE salary > 2000 AND months < 10
 ORDER BY employee_id;
 
-/* Q3. Higher Than 75 Marks)
+/* Higher Than 75 Marks -
 Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. 
 If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID. */
 SELECT Name
@@ -42,7 +42,7 @@ FROM STUDENTS
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID;
 
-/* Q4. Weather Observation Station 15)
+/* Weather Observation Station 15 -
 Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. 
 Round your answer to 4 decimal places. */
 SELECT ROUND(LONG_W, 4)
