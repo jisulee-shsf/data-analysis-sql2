269d6cc5c3456fbd19ddf8ee6379a432ad8a2aa4
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-21 05:11:21 +0900

    Update 02_group_by_220926.sql

--- 02_데이터_분석을_위한_중급_SQL/02_group_by_220926.sql
+++ 02_데이터_분석을_위한_중급_SQL/02_group_by_220926.sql
@@ -1,11 +1,11 @@
 -- [Section 1] GROUP BY
 
--- Q1. SupplierID를 기준으로 평균 Price 출력하기
+# SupplierID를 기준으로 평균 Price 출력하기
 SELECT SupplierID, AVG(Price)
 FROM Products
 GROUP BY SupplierID;
 
--- Q2. SupplierID / CategoryId 기준으로 가격이 높은 순으로 평균 Price 출력하기
+# SupplierID / CategoryId 기준으로 가격이 높은 순으로 평균 Price 출력하기
 SELECT SupplierID
      , CategoryId
      , AVG(Price)
@@ -13,7 +13,7 @@ FROM Products
 GROUP BY SupplierID, CategoryId
 ORDER BY AVG(Price) DESC;
 
--- Q3. SupplierID / CategoryId 기준으로 50 이상의 평균 Price를 'avg_price' 명칭으로 출력하기
+# SupplierID / CategoryId 기준으로 50 이상의 평균 Price를 'avg_price' 명칭으로 출력하기
 SELECT SupplierID
      , CategoryId
      , AVG(Price) AS avg_price
@@ -23,7 +23,7 @@ HAVING AVG(Price) >= 50;
 
 -- [Section 2] HakerRank
 
-/* Q. Top Earners)
+/* Top Earners -
 We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
 Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
 Then print these values as 2 space-separated integers. */
