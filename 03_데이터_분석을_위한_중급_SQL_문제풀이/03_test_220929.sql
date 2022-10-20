072efbd15ff99fe4c76e3be02325ce6955338842
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-21 05:30:30 +0900

    Update 03_test_220929.sql

--- 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
+++ 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
@@ -1,12 +1,12 @@
--- Q1. Population Density Difference)
-/* Query the difference between the maximum and minimum populations in CITY. */
+/* Population Density Difference -
+Query the difference between the maximum and minimum populations in CITY. */
 SELECT MAX(POPULATION) - MIN(POPULATION)
 FROM CITY;
 
--- Q2. Weather Observation Station 11)
-/* Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+/* Weather Observation Station 11 -
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
 Your result cannot contain duplicates. */
--- A1. 기존 코드
+# 기존 코드
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY NOT LIKE 'a%' 
@@ -20,7 +20,7 @@ OR CITY NOT LIKE '%i'
 OR CITY NOT LIKE '%o' 
 OR CITY NOT LIKE '%u';
 
--- A2-1. 수정 코드
+# 수정 코드
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY NOT LIKE 'a%'
@@ -34,21 +34,21 @@ AND CITY NOT LIKE '%i'
 AND CITY NOT LIKE '%o' 
 AND CITY NOT LIKE '%u';
 
--- A2-2. 수정 코드
+# 수정 코드
 SELECT DISTINCT CITY
 FROM STATION
 WHERE LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') 
 OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
 
--- Q3. Weather Observation Station 13)
-/* Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
+/* Weather Observation Station 13 -
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
 Truncate your answer to 4 decimal places. */
 SELECT TRUNCATE(SUM(LAT_N), 4) 
 FROM STATION
 WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
 
--- Q4. Top Competitors)
-/* Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+/* Top Competitors -
+Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
 Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
 Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
 If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id. */
@@ -56,9 +56,9 @@ If more than one hacker received full scores in same number of challenges, then 
 SELECT S.hacker_id
      , H.name
 FROM Submissions S
-     INNER JOIN Hackers H ON S.hacker_id = H.hacker_id
-     INNER JOIN Challenges C ON S.challenge_id = C.challenge_id
-     INNER JOIN Difficulty D ON C.difficulty_level = D.difficulty_level
+INNER JOIN Hackers H ON S.hacker_id = H.hacker_id
+INNER JOIN Challenges C ON S.challenge_id = C.challenge_id
+INNER JOIN Difficulty D ON C.difficulty_level = D.difficulty_level
 WHERE S.score = D.score
 GROUP BY S.hacker_id, H.name
 HAVING COUNT(DISTINCT S.submission_id) >= 2
