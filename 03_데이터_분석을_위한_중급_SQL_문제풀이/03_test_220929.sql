1d1b87a9567069062ce252fe4f5b6c84401ba41f
Jisu Lee <109773795+jisulee-shsf@users.noreply.github.com>
2022-10-24 05:44:40 +0900

    Update 03_test_220929.sql

--- 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
+++ 03_데이터_분석을_위한_중급_SQL_문제풀이/03_test_220929.sql
@@ -52,7 +52,6 @@ Julia just finished conducting a coding contest, and she needs your help assembl
 Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
 Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
 If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id. */
--- 수정 코드
 SELECT S.hacker_id
      , H.name
 FROM Submissions S
